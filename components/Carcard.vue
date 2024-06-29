8734a1323ac5f81ae861575d4b8afea5d9c934af
StoicBug <outidrarinebachir@gmail.com>
2024-06-29 16:58:21 +0100

    Card UI

---
+++ components/Carcard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="font-sans">
+        <img class="w-full border-x border-t border-[#E3E7E8]" src="https://placehold.co/400x250" alt="">
+        <div class="flex items-center justify-between bg-[#F1F8F9] p-5 text-text_primary">
+            <div class="flex flex-col gap3">
+                <h3 class="font-medium uppercase text-text_subheader">NISSAN</h3>
+                <h2 class="text-2xl font-bold tracking-tighter font-display">QASHQAI</h2>
+                <p class="mt-2 text-sm font-medium">1.5 dCi FAP 110ch N-CONNECTA</p>
+                <p class="text-sm">2018 - Essence - 75 ch</p>
+            </div>
+            <div>
+                <p class="text-2xl font-bold tracking-tight">30 400 €</p>
+                <p class="text-sm font-bold"><span class="line-through">12 990 €</span> <span
+                        class="px-1 bg-[#F4AB22]">-33%</span> </p>
+                <p class="text-[#042A38] text-sm font-medium leading-tight mt-2 opacity-60">
+                    <span class="block mb-0.5">Ou à partir de</span>
+                    <span class="text-xl font-semibold block -mt-1 mb-0.5">300 €</span>
+                    <span class="block -mt-1">/mois</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="grid grid-cols-4 gap-5 p-10 ">
-        <aside class="pr-10 ">
-            <h2 class="text-3xl font-semibold tracking-tight font-display">FILTRER MA RECHERCHE</h2>
-
+    <div class="grid h-screen grid-cols-5 gap-5 p-10 font-sans">
+        <aside class="overflow-y-scroll ">
+            <h2 class="text-3xl font-semibold tracking-tighter font-display">FILTRER MA RECHERCHE</h2>
             <div class="flex flex-col gap-4 font-sans mt-7 max-w-64">
                 <div class="flex items-center justify-between px-4 py-3 rounded-md text-white bg-[#18495C]">
                     <p>Prêt à partir</p>
@@ -36,16 +35,22 @@
                 </div>
 
                 <h3 class="mt-5 font-sans text-lg font-medium text-text_primary">Budget</h3>
-                <div class="inline-flex w-48 text-sm leading-none bg-white border border-[#18495C] rounded-lg text-text_primary">
-                    <button class="inline-flex items-center text-white px-6 py-3 transition-colors duration-300 ease-in bg-[#18495C] rounded-l-lg focus:outline-none hover:text-blue-400 focus:text-blue-400 active" id="grid">
-                    <span>Total</span>
+                <div
+                    class="inline-flex w-48 text-sm leading-none bg-white border border-[#18495C] rounded-lg text-text_primary">
+                    <button
+                        class="inline-flex items-center text-white px-6 py-3 transition-colors duration-300 ease-in bg-[#18495C] rounded-l-lg focus:outline-none hover:text-blue-400 focus:text-blue-400 active"
+                        id="grid">
+                        <span>Total</span>
                     </button>
-                    <button class="inline-flex items-center px-6 py-3 transition-colors duration-300 ease-in rounded-r-lg focus:outline-none hover:text-blue-400 focus:text-blue-400" id="list">
-                    <span>Mensuel</span>
+                    <button
+                        class="inline-flex items-center px-6 py-3 transition-colors duration-300 ease-in rounded-r-lg focus:outline-none hover:text-blue-400 focus:text-blue-400"
+                        id="list">
+                        <span>Mensuel</span>
                     </button>
                 </div>
                 <div class="w-full max-w-lg mx-auto text-center">
-                    <input type="range" id="rangeInput" name="rangeInput" min="0" max="5000" step="1" value="50" class="accent-[#18495C] w-full h-2 bg-gray-300 rounded-full appearance-none">
+                    <input type="range" id="rangeInput" name="rangeInput" min="0" max="5000" step="1" value="50"
+                        class="accent-[#18495C] w-full h-2 bg-gray-300 rounded-full appearance-none">
                     <div class="flex justify-between text-sm font-medium text-text_primary">
                         <span id="minValue">100 €</span>
                         <span id="currentValue">2500 €</span>
@@ -53,36 +58,162 @@
                     </div>
                 </div>
 
-                <hr class=" opacity-50 border-[#18495C]">
-                
+                <hr class=" opacity-50 border-[#18495C] mt-3">
                 <div class="flex items-center justify-between ">
-                    <h3 class="mt-5 font-sans text-lg font-medium text-text_primary">Marque</h3>
-                    <svg width="40px" height="40px" class="block" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z" fill="#9e9e9e"></path> </g></svg>                
+                    <h3 class="font-sans text-lg font-medium text-text_primary">Marque</h3>
+                    <svg width="40px" height="40px" class="block" viewBox="0 0 24.00 24.00" fill="none"
+                        xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e" stroke-width="0.00024000000000000003">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+                                fill="#9e9e9e"></path>
+                        </g>
+                    </svg>
                 </div>
                 <div class="flex flex-col gap-2 text-sm">
                     <!-- radio buttons with labels on the left -->
                     <label class="inline-flex items-center">
-                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3] focus:outline-none checked:outline-none" name="marque" value="toutes">
+                        <input type="radio"
+                            class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3] focus:outline-none checked:outline-none"
+                            name="marque" value="toutes">
                         <span class="ml-2 text-text_primary">Toutes</span>
                     </label>
                     <label class="inline-flex items-center">
-                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="marque" value="dacia">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="marque"
+                            value="dacia">
                         <span class="ml-2 text-text_primary">Dacia</span>
                     </label>
                     <label class="inline-flex items-center">
-                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="marque" value="nissan">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="marque"
+                            value="nissan">
                         <span class="ml-2 text-text_primary">Nissan</span>
                     </label>
                     <label class="inline-flex items-center">
-                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="marque" value="renault">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="marque"
+                            value="renault">
                         <span class="ml-2 text-text_primary">Renault</span>
                     </label>
                 </div>
 
+                <hr class=" opacity-50 border-[#18495C]">
+                <div class="flex items-center justify-between ">
+                    <h3 class="font-sans text-lg font-medium text-text_primary">Marque</h3>
+                    <svg width="40px" height="40px" class="block" viewBox="0 0 24.00 24.00" fill="none"
+                        xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e" stroke-width="0.00024000000000000003">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+                                fill="#9e9e9e"></path>
+                        </g>
+                    </svg>
+                </div>
+                <div class="flex flex-col gap-2 text-sm">
+                    <!-- radio buttons with labels on the left -->
+                    <label class="inline-flex items-center">
+                        <input type="radio"
+                            class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3] focus:outline-none checked:outline-none"
+                            name="marque" value="toutes">
+                        <span class="ml-2 text-text_primary">Type de véhicule</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="tous">
+                        <span class="ml-2 text-text_primary">Tous</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="Citadine">
+                        <span class="ml-2 text-text_primary">Citadine</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="berline">
+                        <span class="ml-2 text-text_primary">Berline</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="break">
+                        <span class="ml-2 text-text_primary">Break</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="suv / 4x4">
+                        <span class="ml-2 text-text_primary">SUV / 4x4</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="monospace / ludospace">
+                        <span class="ml-2 text-text_primary">Monospace / Ludospace</span>
+                    </label>
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio h-5 w-5 text-[#70B4C3] accent-[#70B4C3]" name="type"
+                            value="coupé / cabriolet">
+                        <span class="ml-2 text-text_primary">Coupé / Cabriolet</span>
+                    </label>
+                </div>
+
+                <hr class=" opacity-50 border-[#18495C]">
+                <div class="flex items-center justify-between ">
+                    <h3 class="font-sans text-lg font-medium text-text_primary">Kilométrage</h3>
+                    <svg width="40px" height="40px" class="block" viewBox="0 0 24.00 24.00" fill="none"
+                        xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e" stroke-width="0.00024000000000000003">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+                                fill="#9e9e9e"></path>
+                        </g>
+                    </svg>
+                </div>
+
+                <hr class=" opacity-50 border-[#18495C]">
+                <div class="flex items-center justify-between ">
+                    <h3 class="font-sans text-lg font-medium text-text_primary">Couleur</h3>
+                    <svg width="40px" height="40px" class="block" viewBox="0 0 24.00 24.00" fill="none"
+                        xmlns="http://www.w3.org/2000/svg" stroke="#9e9e9e" stroke-width="0.00024000000000000003">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+                                fill="#9e9e9e"></path>
+                        </g>
+                    </svg>
+                </div>
+
             </div>
         </aside>
-        <main class="col-span-3">
-            <p>1356 résultats correspondant à votre recherche</p>
+
+        <main class="col-span-4 text-text_primary">
+            <div class="flex items-center justify-between">
+                <select class="" name="" id="">
+                    <option value="Meilleure affaire">Meilleure affaire</option>
+                    <option value="Plus récent">Plus récent</option>
+                    <option value="Moins cher">Moins cher</option>
+                    <option value="Plus cher">Plus cher</option>
+                </select>
+                <div>
+                    Trier par
+                    <select>
+                        <option>Meilleure affaire</option>
+                        <option>Plus récent</option>
+                        <option>Moins cher</option>
+                        <option>Plus cher</option>
+                    </select>
+                </div>
+            </div>
+            <div class="grid grid-cols-3 gap-10 mt-10">
+                <Carcard />
+                <Carcard />
+                <Carcard />
+                <Carcard />
+
+            </div>
         </main>
     </div>
 </template>
